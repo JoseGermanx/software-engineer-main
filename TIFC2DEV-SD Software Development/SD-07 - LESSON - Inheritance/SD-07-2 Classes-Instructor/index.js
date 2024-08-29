84f178bdc491c3a73d1eefa5a89baa814191f90d
Jose Germán Martínez <jgermanmm@gmail.com>
2024-08-29 10:45:19 -0400

    clases

--- TIFC2DEV-SD Software Development/SD-07 - LESSON - Inheritance/SD-07-2 Classes-Instructor/index.js
+++ TIFC2DEV-SD Software Development/SD-07 - LESSON - Inheritance/SD-07-2 Classes-Instructor/index.js
@@ -1,14 +1,35 @@
-const currentDate = new Date()
+
+/// con el new creamos una nueva instancia de una clase (o de un función constructora)
+const currentDate = new Date() // clae incorparada de JS que nos entrega información de fecha
+
+
+currentDate.getFullYear()
+console.log(currentDate)
 
 class Smartphone {
-  constructor(make, year) {
+
+  constructor(make, year, color) {
     this.make = make
     this.year = year
+    this.color = color
   }
 
-  age() {
-
+  age() {           // 2024
     return currentDate.getFullYear() - this.year
   }
+
+  otroMetodo() {
+    console.log('otro metodo')
+  }
+
 }
 
+const nokia = new Smartphone("Nokia", 2000)
+console.log(nokia)
+
+
+nokia.color = "Space Gray"
+
+console.log(nokia)
+
+const iphone = new Smartphone("Iphone", 2010)
